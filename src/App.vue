659af4b9dc1fe162e562659609f83f040c21d598
Julian O'Grady <julianogrady07@gmail.com>
2023-01-21 00:10:28 -0500

    lang updates

--- src/App.vue
+++ src/App.vue
@@ -7,223 +7,250 @@ import CurtainAnimation from "./components/CurtainAnimation.vue"
 import Topbar from "./components/Topbar.vue"
 import Footer from "./components/Footer.vue"
 import Project from "./components/Project.vue"
+import Language from "./components/PLanguage.vue"
 
 import "./vivus.min.js";
 import Typed from "./typed/typed.js"
 
 let svgItems = ["./src/assets/terminal.svg", "./src/assets/controller.svg", "./src/assets/journal.svg"]
 
-window.addEventListener("load", function(event) {
-  let svgDiv = document.getElementById("item-svg");
+window.addEventListener("load", function (event) {
+	let svgDiv = document.getElementById("item-svg");
 
-  let lastDrawn;
+	let lastDrawn;
 
-  var options = {
-    strings: ["I'm a programmer.","I'm a game developer.","I'm a student."],
-    typeSpeed: 100,
-    backSpeed: 100,
-    startDelay: 4500,
-    smartBackspace: true,
-    showCursor: false,
-    backDelay: 700,
-    preStringTyped: (arrayPos) => {
-      svgDiv.innerHTML = ""
-      lastDrawn = new Vivus("item-svg", {duration:100, file:svgItems[arrayPos], type:"sync"});
-    },
-    onStringTyped: (arrayPos) => {
-      if (arrayPos != svgItems.length-1)
-        lastDrawn.play(-1);
-      else{
-        let downArrow =  document.getElementById("down-arrow");
-        downArrow.setAttribute("style", "")
-        downArrow.classList.add("fade-in");
-      }
-    },
-  };
+	var options = {
+		strings: ["I'm a programmer.", "I'm a game developer.", "I'm a student."],
+		typeSpeed: 100,
+		backSpeed: 100,
+		startDelay: 4500,
+		smartBackspace: true,
+		showCursor: false,
+		backDelay: 700,
+		preStringTyped: (arrayPos) => {
+			svgDiv.innerHTML = ""
+			lastDrawn = new Vivus("item-svg", { duration: 100, file: svgItems[arrayPos], type: "sync" });
+		},
+		onStringTyped: (arrayPos) => {
+			if (arrayPos != svgItems.length - 1)
+				lastDrawn.play(-1);
+			else {
+				let downArrow = document.getElementById("down-arrow");
+				downArrow.setAttribute("style", "")
+				downArrow.classList.add("fade-in");
+			}
+		},
+	};
 
-  new Typed('#typed', options);
+	new Typed('#typed', options);
 
 });
 </script>
 
 <template>
 
-  <Topbar></Topbar>
+	<Topbar></Topbar>
 
-  <CurtainAnimation></CurtainAnimation>
-  <Logo></Logo>
+	<CurtainAnimation></CurtainAnimation>
+	<Logo></Logo>
 
-  <div class="first-page">
-    <h2>Julian O'Grady</h2>
-    <div class="devider"></div>
-    <p id="typed"></p>
-  </div>
-  <div id="item-svg"></div>
-  <img id="down-arrow" src="./assets/down-arrow.svg" style="display:none;">
+	<div class="first-page">
+		<h2>Julian O'Grady</h2>
+		<div class="devider"></div>
+		<p id="typed"></p>
+	</div>
+	<div id="item-svg"></div>
+	<img id="down-arrow" src="./assets/down-arrow.svg" style="display:none;">
 
-  <div class="content" id="about-me">
-    <h2>About me</h2>
-    <div class="devider"></div>
-    <p>Hello, my name is Julian or sometimes known by the allias umi. I am a software engineer & game developer and am currently studying dramatic arts.</p>
-  </div>
+	<div class="content" id="about-me">
+		<h2>About me</h2>
+		<div class="devider"></div>
+		<p>Hello, my name is Julian or sometimes known by the allias umi. I am a software engineer & game developer and
+			am currently studying dramatic arts.</p>
+	</div>
 
-  <div class="content" id="experience">
-    <h2>Experience</h2>
-    <div class="devider"></div>
-    <p>
-      I like to explore many parts of the software engineering field, as such I have experience programming in html, css, javascript, typescript, python, c#, gml, lua, java, and c/c++.
-      <br>
-      Some web frameworks I've used include nodejs, vue, angularjs, and ionic.
-      <br>
-      As well as experience using mujltiple IDEs such as; IntelliJ Idea, Visual Studio, Visual Studio Code, Pycharm, GameMaker Studio 2, and Sublime Text.
-      <br>
-      Extra tools include Git, GitHub, devkitPro, and Unity. 
-    </p>
-  </div>
+	<div class="content" id="experience">
+		<h2>Experience</h2>
+		<div class="devider"></div>
+		<p>
+			I have done a lot of programming work in my own time; because of this I have become proficient in using many development environments. I have experience using Visual Studio, VSCode, Sublime Text, Neovim, Unity, Pycharm, GoLand, and Intellij Idea.
+		</p>
+	</div>
 
-  <div id="projects">
-    <h2>Projects</h2>
-    <div class="devider"></div>
-  </div>
+	<div class="content" id="languages">
+		<h2>Programming Languages</h2>
+		<div class="devider"></div>
+		<div id="language-wrapper">
+			<Language icon="fa-brands fa-golang" name="Golang"></Language>
+			<Language icon="fa-brands fa-python" name="Golang"></Language>
+		</div>
+	</div>
 
-  <div id="project-wrapper">
-    <Project title="Pumpkin Spice" img="./src/assets/pumpkin-spice.png" href="https://um1.itch.io/pumpkin-spice-post-jam">A solo rythmn game made in 3 days for the Cozy Fall Jam 2021 in which it placed 8th overall and 2nd for presentation.</Project>
-    <Project title="It Spreads" img="./src/assets/it-spreads.jpg" href="https://um1.itch.io/it-spreads">A solo puzzle game made in a week for the UW Games Institute virtual game jam where it jointly won the people's choice award.</Project>
-    <Project title="Wave Reader" img="./src/assets/wave-reader.png" href="https://github.com/Umi-L/wave-reader/">An epub reader made using Ionic, Angularjs, EPUB.js, and the dropbox api.</Project>
-    <Project title="Hue-nx" img="./src/assets/hue-nx.jpg" href="https://github.com/Umi-L/hue-nx">A port of the mobile game "I Love Hue" to the nintendo switch using the love potion framework.</Project>
-    <Project title="Slather" img="./src/assets/slather.png" href="https://github.com/Umi-L/slather-client/tree/master">A slither.io clone made to learn websocket using typescript & nodejs for the backend and typescript & canvas for the frontend.</Project>
-  </div>
+	<div id="projects">
+		<h2>Projects</h2>
+		<div class="devider"></div>
+	</div>
 
-  <Footer></Footer>
+	<div id="project-wrapper">
+		<Project title="Pumpkin Spice" img="./src/assets/pumpkin-spice.png"
+			href="https://um1.itch.io/pumpkin-spice-post-jam">A solo rythmn game made in 3 days for the Cozy Fall Jam
+			2021 in which it placed 8th overall and 2nd for presentation.</Project>
+		<Project title="It Spreads" img="./src/assets/it-spreads.jpg" href="https://um1.itch.io/it-spreads">A solo
+			puzzle game made in a week for the UW Games Institute virtual game jam where it jointly won the people's
+			choice award.</Project>
+		<Project title="Wave Reader" img="./src/assets/wave-reader.png" href="https://github.com/Umi-L/wave-reader/">An
+			epub reader made using Ionic, Angularjs, EPUB.js, and the dropbox api.</Project>
+		<Project title="Hue-nx" img="./src/assets/hue-nx.jpg" href="https://github.com/Umi-L/hue-nx">A port of the
+			mobile game "I Love Hue" to the nintendo switch using the love potion framework.</Project>
+		<Project title="Slather" img="./src/assets/slather.png"
+			href="https://github.com/Umi-L/slather-client/tree/master">A slither.io clone made to learn websocket using
+			typescript & nodejs for the backend and typescript & canvas for the frontend.</Project>
+	</div>
+
+	<Footer></Footer>
 </template>
 
 <style>
-  .stop-scroll{
-    overflow-y: hidden;
-  }
+.stop-scroll {
+	overflow-y: hidden;
+}
 
-  @keyframes bob {
-    0%, 100% {
-      transform: translate(-50%, 0px);
-    }
-    50% {
-      transform: translate(-50%, -10px);
-    }
-  }
-  @keyframes fade-in {
-    0%{
-      opacity: 0%;
-    }
-    100%{
-      opacity: 100%;
-    }
-  }
+@keyframes bob {
 
-  body{
-    margin: 0%;
+	0%,
+	100% {
+		transform: translate(-50%, 0px);
+	}
 
-    overflow-x: hidden;
-    font-family: 'Roboto', sans-serif;
-  }
+	50% {
+		transform: translate(-50%, -10px);
+	}
+}
 
-  .first-page{
-    margin-left: auto;
-    margin-right: auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    height: 100vh;
-    width: 60%;
-  }
+@keyframes fade-in {
+	0% {
+		opacity: 0%;
+	}
 
-  #project-wrapper{
-    display: flex;
+	100% {
+		opacity: 100%;
+	}
+}
 
-    flex-wrap: wrap;
-    flex-direction: row;
+body {
+	margin: 0%;
 
-    justify-content: center;
-  }
+	overflow-x: hidden;
+	font-family: 'Roboto', sans-serif;
+}
 
-  #projects{
-    font-size: 2em;
-    
-    text-align: center;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-  }
+.first-page {
+	margin-left: auto;
+	margin-right: auto;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	height: 100vh;
+	width: 60%;
+}
 
-  .content{
-    margin-left: auto;
-    margin-right: auto;
-    display: flex;
-    flex-direction: column;
-    width: 60%;
-    margin-bottom: 100px;
-  }
+#project-wrapper {
+	display: flex;
 
+	flex-wrap: wrap;
+	flex-direction: row;
 
-  .underlined{
-    border-bottom: 1px solid rgb(195, 195, 195);
-  }
+	justify-content: center;
+}
 
-  .first-page h2{
-    font-size: 3em;
-  }
-  .first-page p{
-    font-size: 1.5em;
-    height:1.5em
-  }
+#language-wrapper {
+	display: flex;
 
-  .content h2{
-    font-size: 3em;
-  }
-  .content p{
-    font-size: 1.5em;
-    line-height: 1.3em;
-  }
+	flex-wrap: wrap;
+	flex-direction: row;
+
+	justify-content: left;
+}
+
+#projects {
+	font-size: 2em;
+
+	text-align: center;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+}
+
+.content {
+	margin-left: auto;
+	margin-right: auto;
+	display: flex;
+	flex-direction: column;
+	width: 60%;
+	margin-bottom: 100px;
+}
 
 
-  .devider{
-    width: 10%;
-    border-bottom: 1px black solid;
-  }
+.underlined {
+	border-bottom: 1px solid rgb(195, 195, 195);
+}
 
-  #down-arrow{
-    position: absolute;
-    top: 90%;
-    left: 50%;
-    width: 70px;
+.first-page h2 {
+	font-size: 3em;
+}
 
-    transform: translateX(-50%);
-  }
+.first-page p {
+	font-size: 1.5em;
+	height: 1.5em
+}
 
-  .fade-in{
-    animation: fade-in 2s ease-out forwards, bob 1s ease-in-out infinite;
-  }
-  
+.content h2 {
+	font-size: 3em;
+}
 
-  @media screen and (min-width: 750px) {
-    #item-svg{
-      width: 25%;
-      position:absolute;
-      top: 50vh;
-      right: 20vw;
-
-      transform: translate(50%, -50%);
-    }
-  }
-
-  @media not screen and (min-width: 750px) {
-    #item-svg{
-      display: none;
-    }
-    .devider{
-      width: 100px;
-    }
-  }
-  
+.content p {
+	font-size: 1.5em;
+	line-height: 1.3em;
+}
 
 
+.devider {
+	width: 10%;
+	border-bottom: 1px black solid;
+}
+
+#down-arrow {
+	position: absolute;
+	top: 90%;
+	left: 50%;
+	width: 70px;
+
+	transform: translateX(-50%);
+}
+
+.fade-in {
+	animation: fade-in 2s ease-out forwards, bob 1s ease-in-out infinite;
+}
+
+
+@media screen and (min-width: 750px) {
+	#item-svg {
+		width: 25%;
+		position: absolute;
+		top: 50vh;
+		right: 20vw;
+
+		transform: translate(50%, -50%);
+	}
+}
+
+@media not screen and (min-width: 750px) {
+	#item-svg {
+		display: none;
+	}
+
+	.devider {
+		width: 100px;
+	}
+}
 </style>
---
+++ src/components/PLanguage.vue
@@ -0,0 +1,44 @@
+<!-- BlogPost.vue -->
+<script>
+export default {
+    props: ['icon', 'name'],
+}
+
+</script>
+
+<template>
+    <div class="lang-container">
+        <i :class=icon></i>
+        <p class="small-font">{{ name }}</p>
+    </div>
+</template>
+
+<style>
+    .lang-container{
+
+        border: 1px black solid;
+        border-radius: 7px;
+
+        height: 20%;
+        width: 20%;
+
+        margin: 2%;
+        padding: 1%;
+
+        overflow: hidden;
+
+        transition: 0.2s;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    i{
+        font-size: 5vw;
+        
+    }
+    .small-font{
+        font-size: 0.8em !important;
+    }
+</style>
